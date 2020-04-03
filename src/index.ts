e276b9087eba4271b29252efda83195780087b3f
taoqf <tao_qiufeng@126.com>
2020-04-03 10:42:50 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 
-export default function awx17(param1: string, param2: string, param3: string) {
+export default function choose_image(count: number, sizeType: wx.ImageSizeType[], sourceType: wx.ImageSourceType[]) {
+	return new Promise<wx.TempFilesData | null>((resolve) => {
+		wx.chooseImage({
+			count, 						// 默认9
+			sizeType, 		// 可以指定是原图还是压缩图，默认二者都有
+			sourceType,	// 可以指定来源是相册还是相机，默认二者都有
+			success(res) {				// 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+				resolve(res);
+			},
+			fail() {
+				resolve(null);
+			}
+		});
+	});
 }
